acfe9fd3258c3176534538ee5c63b1b920a7f364
Elder <elderorama@gmail.com>
2020-03-30 01:39:30 -0300

    Primeiro Commit

---
+++ Thomas Greg/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Thomas_Greg
+{
+    partial class frmCadastroCliente
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastroCliente));
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pcImagem = new System.Windows.Forms.PictureBox();
+            this.btnCarregarImagem = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbNovo = new System.Windows.Forms.ToolStripButton();
+            this.tsbSalvar = new System.Windows.Forms.ToolStripButton();
+            this.tsbCancelar = new System.Windows.Forms.ToolStripButton();
+            this.tsbExcluir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.tstId = new System.Windows.Forms.ToolStripTextBox();
+            this.tsbBuscar = new System.Windows.Forms.ToolStripButton();
+            this.ofd1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pcImagem)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(23, 46);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(55, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(115, 46);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(251, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtEndereco.Location = new System.Drawing.Point(23, 99);
+            this.txtEndereco.MaxLength = 50;
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(251, 20);
+            this.txtEndereco.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Endereço :";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(309, 99);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(142, 20);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(306, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "E-mail :";
+            // 
+            // pcImagem
+            // 
+            this.pcImagem.Location = new System.Drawing.Point(492, 28);
+            this.pcImagem.Name = "pcImagem";
+            this.pcImagem.Size = new System.Drawing.Size(111, 107);
+            this.pcImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcImagem.TabIndex = 8;
+            this.pcImagem.TabStop = false;
+            // 
+            // btnCarregarImagem
+            // 
+            this.btnCarregarImagem.Location = new System.Drawing.Point(492, 141);
+            this.btnCarregarImagem.Name = "btnCarregarImagem";
+            this.btnCarregarImagem.Size = new System.Drawing.Size(111, 23);
+            this.btnCarregarImagem.TabIndex = 9;
+            this.btnCarregarImagem.Text = "Carregar Imagem";
+            this.btnCarregarImagem.UseVisualStyleBackColor = true;
+            this.btnCarregarImagem.Click += new System.EventHandler(this.BtnCarregarImagem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbNovo,
+            this.tsbSalvar,
+            this.tsbCancelar,
+            this.tsbExcluir,
+            this.toolStripSeparator1,
+            this.toolStripLabel,
+            this.tstId,
+            this.tsbBuscar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(681, 25);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = " ";
+            // 
+            // tsbNovo
+            // 
+            this.tsbNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNovo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNovo.Image")));
+            this.tsbNovo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNovo.Name = "tsbNovo";
+            this.tsbNovo.Size = new System.Drawing.Size(23, 22);
+            this.tsbNovo.Text = "Novo";
+            this.tsbNovo.Click += new System.EventHandler(this.TsbNovo_Click);
+            // 
+            // tsbSalvar
+            // 
+            this.tsbSalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSalvar.Image = ((System.Drawing.Image)(resources.GetObject("tsbSalvar.Image")));
+            this.tsbSalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSalvar.Name = "tsbSalvar";
+            this.tsbSalvar.Size = new System.Drawing.Size(23, 22);
+            this.tsbSalvar.Text = "Salvar";
+            this.tsbSalvar.Click += new System.EventHandler(this.TsbSalvar_Click);
+            // 
+            // tsbCancelar
+            // 
+            this.tsbCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbCancelar.Image = ((System.Drawing.Image)(resources.GetObject("tsbCancelar.Image")));
+            this.tsbCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCancelar.Name = "tsbCancelar";
+            this.tsbCancelar.Size = new System.Drawing.Size(23, 22);
+            this.tsbCancelar.Text = "Cancelar";
+            this.tsbCancelar.Click += new System.EventHandler(this.TsbCancelar_Click);
+            // 
+            // tsbExcluir
+            // 
+            this.tsbExcluir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbExcluir.Image = ((System.Drawing.Image)(resources.GetObject("tsbExcluir.Image")));
+            this.tsbExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbExcluir.Name = "tsbExcluir";
+            this.tsbExcluir.Size = new System.Drawing.Size(23, 22);
+            this.tsbExcluir.Text = "Excluir";
+            this.tsbExcluir.Click += new System.EventHandler(this.TsbExcluir_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel
+            // 
+            this.toolStripLabel.Name = "toolStripLabel";
+            this.toolStripLabel.Size = new System.Drawing.Size(105, 22);
+            this.toolStripLabel.Text = "Buscar por Código";
+            // 
+            // tstId
+            // 
+            this.tstId.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tstId.Name = "tstId";
+            this.tstId.Size = new System.Drawing.Size(100, 25);
+            // 
+            // tsbBuscar
+            // 
+            this.tsbBuscar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbBuscar.Image = ((System.Drawing.Image)(resources.GetObject("tsbBuscar.Image")));
+            this.tsbBuscar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBuscar.Name = "tsbBuscar";
+            this.tsbBuscar.Size = new System.Drawing.Size(23, 22);
+            this.tsbBuscar.Text = "Buscar";
+            this.tsbBuscar.Click += new System.EventHandler(this.TsbBuscar_Click);
+            // 
+            // ofd1
+            // 
+            this.ofd1.FileName = "openFileDialog1";
+            // 
+            // frmCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 176);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnCarregarImagem);
+            this.Controls.Add(this.pcImagem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Name = "frmCadastroCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro Cliente";
+            this.Load += new System.EventHandler(this.FrmCadastroCliente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcImagem)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pcImagem;
+        private System.Windows.Forms.Button btnCarregarImagem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbNovo;
+        private System.Windows.Forms.ToolStripButton tsbSalvar;
+        private System.Windows.Forms.ToolStripButton tsbCancelar;
+        private System.Windows.Forms.ToolStripButton tsbExcluir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel;
+        private System.Windows.Forms.ToolStripTextBox tstId;
+        private System.Windows.Forms.ToolStripButton tsbBuscar;
+        private System.Windows.Forms.OpenFileDialog ofd1;
+    }
+}
+
---
+++ Thomas Greg/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security;
+
+namespace Thomas_Greg
+{
+    public partial class frmCadastroCliente : Form
+    {
+        string connectionString = @"Server=.;Database=bdcadastro;Trusted_Connection=True;";
+        bool novo;
+        public frmCadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCadastroCliente_Load(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstId.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            txtEmail.Enabled = false;
+            
+        }
+
+        private void TsbNovo_Click(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = false;
+            tsbSalvar.Enabled = true;
+            tsbCancelar.Enabled = true;
+            tsbExcluir.Enabled = false;
+            tstId.Enabled = false;
+            tsbBuscar.Enabled = false;
+            txtNome.Enabled = true;
+            txtEndereco.Enabled = true;
+            txtEmail.Enabled = true;
+            txtNome.Focus();
+            novo = true;
+        }
+
+        private void TsbSalvar_Click(object sender, EventArgs e)
+        {
+            if (novo)
+            {
+                string sql = "INSERT INTO cliente (NOME,ENDERECO,EMAIL,IMAGE) " +
+     "VALUES (@Nome, @Endereco, @email, @image)";
+
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@Endereco", txtEndereco.Text);
+                cmd.Parameters.AddWithValue("@email", txtEndereco.Text);
+                cmd.Parameters.AddWithValue("@image", pcImagem.Image);
+
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                        MessageBox.Show("Registro incluido com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            else
+            {
+                string sql = "UPDATE BDCADASTRO SET NOME=@Nome, ENDERECO= @Endereco, EMAIL=@email, IMAGEM=@image WHERE ID=@Id";
+
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Id", txtId.Text);
+                cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@Endereco", txtEndereco.Text);
+                cmd.Parameters.AddWithValue("@image", pcImagem.Image);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                        MessageBox.Show("Registro atualizado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstId.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            txtEmail.Enabled = false;
+            txtId.Text = "";
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtEmail.Text = "";
+            
+        }
+
+        private void TsbCancelar_Click(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstId.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            
+            txtId.Text = "";
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtEmail.Text = "";
+            
+        }
+
+        private void BtnCarregarImagem_Click(object sender, EventArgs e)
+        {
+            this.ofd1.Multiselect = false;
+            this.ofd1.Title = "Selecionar Fotos";
+            OpenFileDialog foto = new OpenFileDialog();
+            if (foto.ShowDialog() == DialogResult.OK)
+            {
+                pcImagem.Image = Image.FromFile(foto.FileName);
+            }
+
+
+        }
+
+        private void TsbExcluir_Click(object sender, EventArgs e)
+        {
+            string sql = "DELETE FROM CLIENTE WHERE ID=@Id";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Id", txtId.Text);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    MessageBox.Show("Registro excluído com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstId.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            txtEmail.Enabled = false;
+            txtId.Text = "";
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtEmail.Text = "";
+            
+        }
+
+        private void TsbBuscar_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM CLIENTE WHERE ID=@Id";
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Id", tstId.Text);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader;
+            con.Open();
+            try
+            {
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    tsbNovo.Enabled = false;
+                    tsbSalvar.Enabled = true;
+                    tsbCancelar.Enabled = true;
+                    tsbExcluir.Enabled = true;
+                    tstId.Enabled = false;
+                    tsbBuscar.Enabled = false;
+                    txtNome.Enabled = true;
+                    txtEndereco.Enabled = true;
+                    txtEmail.Enabled = true;
+                    
+                    txtNome.Focus();
+                    txtId.Text = reader[0].ToString();
+                    txtNome.Text = reader[1].ToString();
+                    txtEndereco.Text = reader[2].ToString();
+                    txtEmail.Text = reader[3].ToString();
+                    novo = false;
+                }
+                else
+                    MessageBox.Show("Nenhum registro encontrado com o Id informado!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            tstId.Text = "";
+        }
+    }
+}
